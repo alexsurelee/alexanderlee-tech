2f5156d1c81871f07942cf6d47a05925326dfbe9
Alexander Lee <alexgrlee@gmail.com>
2026-06-04 12:04:48 +1200

    add font and logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const departureMono = localFont({ src: "../../public/departure-mono.woff2" });
 
 export const metadata: Metadata = {
   title: "alexanderlee.tech | Alexander Lee",
   description: "Writings about software design, technology, and more.",
+  icons: "logo-simple-small.svg",
 };
 
 export default function RootLayout({
@@ -23,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" className={`${departureMono.className}`}>
       <body>{children}</body>
     </html>
   );
