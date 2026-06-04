7a7ca20e29a106ad2f2108ce9909b4506fc4a8f5
Alexander Lee <alexgrlee@gmail.com>
2026-06-04 14:51:34 +1200

    some light layout

--- src/app/components/base-layout/base-layout.tsx
+++ src/app/components/base-layout/base-layout.tsx
@@ -1,18 +1,29 @@
 import Image from "next/image";
 import Link from "next/link";
 import { TypewriterTitle } from "@/app/components/typewriter-title";
-import styles from "./layout.module.css";
+import styles from "./base-layout.module.css";
 
 export function BaseLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <nav>
-        <Link href={"/"} className={styles.homeLink}>
-          <Image src={"logo-small.svg"} width="50" height={"50"} alt={"Home"} />
-          <TypewriterTitle />
-        </Link>
-      </nav>
-      <main>{children}</main>
-    </>
+    <div className={styles.layout}>
+      <header className={styles.header}>
+        <nav className={styles.nav}>
+          <Link href={"/"} className={styles.homeLink}>
+            <Image
+              src={"logo-small.svg"}
+              width="50"
+              height={"50"}
+              alt={"Home"}
+            />
+            <TypewriterTitle />
+          </Link>
+        </nav>
+      </header>
+      <div className={styles.body}>
+        <div className={styles.leftGutter} aria-hidden="true" />
+        <main className={styles.main}>{children}</main>
+        <div className={styles.rightGutter} aria-hidden="true" />
+      </div>
+    </div>
   );
 }
