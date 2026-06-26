b11fbdc06a9bfabaff5dd918c2ace88e3ed2a42b
Alexander Lee <alexgrlee@gmail.com>
2026-06-27 08:58:10 +1200

    name only in title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { AppHotkeysProvider } from "@/app/components/hotkeys-provider";
 import { atkinson, monaspace } from "./fonts";
 
 export const metadata: Metadata = {
-  title: "lee.computer | Alexander Lee",
+  title: "Alexander Lee",
   description: "Writings about software design, technology, and more.",
   icons: "/logo-simple-small.svg",
 };
