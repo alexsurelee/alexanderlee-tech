eb537ebdebcff7e7759d972bc30ad8be38d7a632
Alexander Lee <alexgrlee@gmail.com>
2026-06-27 10:39:29 +1200

    unneeded config

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,6 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
-    environmentMatchGlobs: [["scripts/**", "node"]],
     globals: false,
     setupFiles: ["./vitest-setup.ts"],
   },
